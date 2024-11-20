7d96d08f829664484fe9900fc9cb05029de5be15
harmeet.singh <harmeet.singh@coxautoinc.com>
2024-11-20 14:46:57 -0500

    updating model file to fix tests

--- model.py
+++ model.py
@@ -5,15 +5,15 @@ import torch.nn.functional as F
 class MNISTModel(nn.Module):
     def __init__(self):
         super(MNISTModel, self).__init__()
-        self.conv1 = nn.Conv2d(1, 16, kernel_size=3, padding=1)
-        self.conv2 = nn.Conv2d(16, 32, kernel_size=3, padding=1)
-        self.fc1 = nn.Linear(32 * 7 * 7, 128)
-        self.fc2 = nn.Linear(128, 10)
+        self.conv1 = nn.Conv2d(1, 8, kernel_size=3, padding=1)
+        self.conv2 = nn.Conv2d(8, 16, kernel_size=3, padding=1)
+        self.fc1 = nn.Linear(16 * 7 * 7, 64)
+        self.fc2 = nn.Linear(64, 10)
         
     def forward(self, x):
         x = F.max_pool2d(F.relu(self.conv1(x)), 2)
         x = F.max_pool2d(F.relu(self.conv2(x)), 2)
-        x = x.view(-1, 32 * 7 * 7)
+        x = x.view(-1, 16 * 7 * 7)
         x = F.relu(self.fc1(x))
         x = self.fc2(x)
         return x 
